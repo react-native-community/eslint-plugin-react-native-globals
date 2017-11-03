982e3a844c04ca8407940b972918d3896c0cb3a3
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-11-03 19:48:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const globals = {
+  __DEV__: false,
+  __dirname: false,
+  alert: false,
+  Blob: false,
+  cancelAnimationFrame: false,
+  cancelIdleCallback: false,
+  clearImmediate: true,
+  clearInterval: false,
+  clearTimeout: false,
+  console: false,
+  escape: false,
+  Event: false,
+  EventTarget: false,
+  exports: false,
+  fetch: false,
+  File: false,
+  FileReader: false,
+  FormData: false,
+  global: false,
+  Map: true,
+  module: false,
+  process: false,
+  Promise: true,
+  requestAnimationFrame: true,
+  requestIdleCallback: true,
+  require: false,
+  Set: true,
+  setImmediate: true,
+  setInterval: false,
+  setTimeout: false,
+  window: false,
+  XMLHttpRequest: false,
+};
+
+module.exports = {
+  environments: {
+    all: {
+      globals: globals,
+    },
+  },
+};
